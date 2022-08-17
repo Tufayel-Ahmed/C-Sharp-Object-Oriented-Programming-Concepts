7db29bc1b682aa5ee35a2d40ad46de582efd50cb
Tufayel Ahmed <tufayelahmed.edu@gmail.com>
2022-08-17 13:04:52 +0600

    Properties And Constructor Problem Commit

---
+++ PropertiesAndConstructor/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertiesAndConstructor
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+
+        public Person(string name, int age, string address) : this(name, age)    
+        {
+            Name = name;
+            Age = age;
+            Address = address;
+        }
+        public Person(string name, int age) : this()
+        {
+            Name = name;
+            Age = age;
+        }
+        public Person()
+        {
+            
+        }
+
+    }
+}
---
+++ PropertiesAndConstructor/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PropertiesAndConstructor
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            Person person = new Person("Tufayel Ahmed", 27, "Dhaka");
+            Person person1 = new Person("Tufayel Ahmed", 27);
+            Person person2 = new Person();
+        }
+    }
+}
