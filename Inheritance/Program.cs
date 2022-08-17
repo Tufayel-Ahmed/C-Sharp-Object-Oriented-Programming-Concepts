a606ba629620c49f0e1ef6b961b7f53839d7b2f7
Tufayel Ahmed <tufayelahmed.edu@gmail.com>
2022-08-17 23:24:04 +0600

    Inheritance Problem Commit

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Inheritance
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            SavingAccount accountSaving = new SavingAccount();
+            accountSaving.AccountNo = "101";
+            accountSaving.CustomerName = "Tufayel";
+            accountSaving.InterestAmount = 500.0;
+            accountSaving.Deposit(1000);
+            accountSaving.Withdraw(500);
+
+            double balance = accountSaving.Balance;
+            
+            CheckingAccount accountChecking = new CheckingAccount();
+            accountChecking.AccountNo = "102";
+            accountChecking.CustomerName = "Kalam";
+            accountChecking.ServiceCharge = 200.0;
+            accountChecking.Deposit(2000);
+            accountChecking.Withdraw(1000);
+
+            balance = accountChecking.Balance;
+
+            BankAccount bankAccount = new BankAccount();
+            
+            BankAccount bankAccount2 = new SavingAccount();
+            
+            SavingAccount savingAccount = (SavingAccount) new BankAccount();
+
+            BankAccount bankAccount3 = new CheckingAccount();
+        }
+    }
+}
---
+++ Inheritance/SavingAccount.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public class SavingAccount : BankAccount
+    {
+        public double InterestAmount { get; set; }
+
+        public override string Withdraw(double amount)
+        {
+            if(Balance >= amount)
+            {
+                return base.Withdraw(amount);
+            }
+            return "Insufficient Balance";
+        }
+    }
+}
