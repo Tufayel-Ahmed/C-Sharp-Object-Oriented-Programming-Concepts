ff39efcc7469fc48679a8bad7702e17a62d9d07e
Tufayel Ahmed <tufayelahmed.edu@gmail.com>
2022-08-17 11:38:38 +0600

    Value and Reference Type Class and Object Data Hiding Problem Commit

---
+++ ValueandReferenceTypeClassandObjectDataHiding/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ValueandReferenceTypeClassandObjectDataHiding
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            int num = 10;
+            ChangeValueType(num);
+            Console.WriteLine(num);
+
+            Person person = new Person();
+            person.firstName = "Abul";
+            person.middleName = "Kalam";
+            person.lastName = "Azad";
+
+            ChangeReferenceType(person);
+
+            Console.WriteLine(person.firstName);
+            Console.WriteLine(person.middleName);
+            Console.WriteLine(person.lastName);
+
+        }
+
+        public static void ChangeValueType(int n)
+        {
+            n = 20;
+        }
+
+        public static void ChangeReferenceType(Person p)
+        {
+            p.firstName = "Md";
+            p.middleName = "Abul";
+            p.lastName = "Kalam";
+        }
+    }
+}
