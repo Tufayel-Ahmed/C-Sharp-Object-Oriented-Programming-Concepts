0ccd61f6f85503f0affe6f5b63a2c33341bce0d2
Tufayel Ahmed <tufayelahmed.edu@gmail.com>
2022-08-18 15:44:22 +0600

    Interface Problem Commit

---
+++ InterfaceExample/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace InterfaceExample
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            Student student = new Student();
+            student.RegNo = "14177";
+
+            string info = student.GetBasicInformation();
+
+            Department department = new Department();
+            department.Code = "CSE";
+            string deInfo = department.GetBasicInformation();
+
+            Course course = new Course();
+            string coInfo = course.GetBasicInformation();
+        }
+    }
+}
---
+++ InterfaceExample/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceExample
+{
+    public class Student : IInformation, IPrinter
+    {
+        public string RegNo { get; set; }
+        public string Name { get; set; }
+        public string Roll { get; set; }
+        public string Email { get; set; }
+
+        public string GetBasicInformation()
+        {
+            return $"Reg No: {RegNo} Name: {Name} Roll: {Roll}";
+        }
+
+        public void Print()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetIPAddress(string ip)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
